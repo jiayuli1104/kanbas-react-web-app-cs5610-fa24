01bbccf8facb92affe938e5537703dffc36767dc
Jiayu Li <li.jiayu4@northeastern.edu>
2024-10-02 16:07:05 -0700

    a1 update for regrade

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -6,7 +6,7 @@ export default function Lab1() {
       <div id="wd-h-tag">
         <h4>Heading Tags</h4>
         Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
-        <h4>Paragraphs</h4>
+        <h4>Paragraph Tags</h4>
         <p id="wd-p-1">
           This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag
         </p>
@@ -99,11 +99,47 @@ export default function Lab1() {
               <td>2/24/21</td>
               <td>92</td>
             </tr>
+            <tr>
+              <td>Q5</td>
+              <td>Bootstrap</td>
+              <td>3/3/21</td>
+              <td>91</td>
+            </tr>
+            <tr>
+              <td>Q6</td>
+              <td>Flex</td>
+              <td>3/10/21</td>
+              <td>95</td>
+            </tr>
+            <tr>
+              <td>Q7</td>
+              <td>Routing</td>
+              <td>3/17/21</td>
+              <td>98</td>
+            </tr>
+            <tr>
+              <td>Q8</td>
+              <td>State</td>
+              <td>3/24/21</td>
+              <td>94</td>
+            </tr>
+            <tr>
+              <td>Q9</td>
+              <td>Redux</td>
+              <td>3/31/21</td>
+              <td>96</td>
+            </tr>
+            <tr>
+              <td>Q10</td>
+              <td>Node</td>
+              <td>4/7/21</td>
+              <td>97</td>
+            </tr>
           </tbody>
           <tfoot>					{/* table row */}
             <tr>
               <td colSpan={3}>Average</td>	{/* column span */}
-              <td>90</td>
+              <td>93</td>
             </tr>
           </tfoot>
         </table>
